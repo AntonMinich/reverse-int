d1d9e5637fb4d340a07eac1c84bc6bdfe75c6f65
AntonMinich <ant.minich@gmail.com>
2023-08-03 20:54:48 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(n).split('');
+    let arrReverse = [];
+    let newArr = '';
+    let result = '';
+    if (arr[0] !== '-') {
+        arrReverse = arr.reverse();
+        newArr = arrReverse.join('');
+        result = Number(newArr);
+    } else {
+       arr.shift();
+       arrReverse = arr.reverse();
+       newArr = arrReverse.join('');
+       result = Number(newArr);
+    }
+    return result;
 }
